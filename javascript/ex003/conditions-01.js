3a9356f091d87465cbdcd8283ad3aaed4fa8f5a0
ThiagoFrancx <96830098+ThiagoFrancx@users.noreply.github.com>
2022-07-18 08:22:50 -0300

    Ajustes

--- javascript/ex003/conditions-01.js
+++
@@ -1,8 +0,0 @@
-var vel = 70.5
-console.log(`A velocidade do seu carro é ${vel}`) 
-
-if(vel > 60 ){ //Condição Simples!
-    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-}
-
-console.log('Dirija sempre usando cinto')
